bac1bd45483b370aa74267ec296352ebd6638e08
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-06-27 01:32:57 +0300

    for ... of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arr2 = [];
   for (const value of array) {
-    arr2.push( appendString + value);
+    arr2.push(appendString + value);
   }
 
   return arr2;
