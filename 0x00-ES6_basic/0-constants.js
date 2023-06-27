b62dd6e92d8635d2e49f0fc9927df6d2ac8eb21b
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-06-27 13:36:02 +0300

    lint pass

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,15 @@
-/*eslint linebreak-style: ["error", "windows"]*/
 export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
+    const task = 'I prefer const when I can.';
+    return task;
 }
 
 export function getLast() {
-  return ' is okay';
+    return ' is okay';
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
+    let combination = 'But sometimes let';
+    combination += getLast();
   
-  return combination;
+    return combination;
 }
