40a5609181e996bb72eb56bb14e3d0ad2304f8c9
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-06-28 19:08:39 +0300

    handling multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      console.log(results[0].body, results[1].firstName, results[1].lastName);
+  return uploadPhoto()
+    .then((data1) => {
+      createUser()
+        .then((data2) => {
+          console.log(data1.body, data2.firstName, data2.lastName);
+        })
+        .catch(() => console.log('Signup system offline'));
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
