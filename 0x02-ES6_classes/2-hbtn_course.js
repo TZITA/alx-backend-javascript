a734fe500a5b03d65a725170adb0eefeaf23e04a
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-06-29 19:00:57 +0300

    Getter and setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) !== 'string') {
+      throw TypeError('Name must be a string');
+    } else if (typeof (length) !== 'number') {
+      throw TypeError('Length must be a number');
+    } else {
+      this._name = name;
+      this._length = length;
+      this._students = students;
+    }
+  }
+
+  // Getter name
+  get name() {
+    return this._name;
+  }
+
+  // Setter name
+  set name(newName) {
+    if (typeof (newName) !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Getter length
+  get length() {
+    return this._length;
+  }
+
+  // Setter length
+  set length(newLength) {
+    if (typeof (newLength) !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // Getter students
+  get students() {
+    return this._students;
+  }
+
+  // Setter students
+  set students(newStu) {
+    this._students = newStu;
+  }
+}
