4e2475cdb4e7694951b25179056e8b9268ac6248
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-05 23:04:22 +0300

    jest test pass

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const counter = weakMap.get(endpoint) || 0;
 
-  if (counter >= 5) {
+  if (counter >= 4) {
     throw Error('Endpoint load is high');
   }
   weakMap.set(endpoint, counter + 1);
