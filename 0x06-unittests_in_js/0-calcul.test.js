c89b80f0bb09fd1b43eec2eb04ede1d7683d844f
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-23 19:02:10 +0300

    Init commit

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
+
+describe("calculateNumber", function () {
+  it("should return the rounded sum of two integers", function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it("should return the rounded sum of an integer and a decimal number", function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it("should return the rounded sum of two decimal numbers", function () {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it("should return the rounded sum of two decimal numbers", function () {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
